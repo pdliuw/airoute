dfc2e6b951fec6574f3d85bd2c6faed1251a40cc
Air <xkrossiapd163@163.com>
2020-01-13 14:33:31 +0800

    Improve:  Improve health

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## [0.1.6] - Route.
+
+* Improve:  Improve health
 
 ## [0.1.5] - Route.
 
--- lib/airoute.dart
+++ lib/airoute.dart
@@ -6,7 +6,6 @@ import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 
 ///
 /// Part
